da71f26ce4ea11b7cc7d5954843a5913c1218f08
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-02-14 10:06:47 -0300

    by Micael

--- www/js/js_forca.js
+++ www/js/js_forca.js
@@ -3,20 +3,30 @@ const wordArray = word.toLowerCase().split("");
 const incorrectElement = document.getElementById("incorrect");
 const guessElement = document.getElementById("guess");
 const wordElement = document.getElementById("word");
+const letterButtonsContainer = document.getElementById("letter-buttons");
 let incorrect = [];
 let correct = [];
+
+// create a button for each letter of the alphabet
+for (let i = 97; i <= 122; i++) {
+  const letter = String.fromCharCode(i);
+  const button = document.createElement("button");
+  button.textContent = letter;
+  button.addEventListener("click", function() {
+    handleGuess(letter);
+  });
+  letterButtonsContainer.appendChild(button);
+}
 
 wordArray.forEach(function() {
   correct.push("_");
 });
 wordElement.innerHTML = correct.join(" ");
 
-document.getElementById("submit").addEventListener("click", function() {
-  const letter = document.getElementById("letter").value.toLowerCase();
-  
+function handleGuess(letter) {
   if (wordArray.indexOf(letter) === -1) {
     incorrect.push(letter);
-    incorrectElement.innerHTML = "Incorrect: " + incorrect.join(" ");
+    incorrectElement.innerHTML = "Incorretas: " + incorrect.join(" ");
   } else {
     for (let i = 0; i < wordArray.length; i++) {
       if (wordArray[i] === letter) {
@@ -35,4 +45,4 @@ document.getElementById("submit").addEventListener("click", function() {
     guessElement.innerHTML = "You lose.";
     guessElement.style.color = "red";
   }
-})
+}
---
+++ www/js/js_velha.js
@@ -0,0 +1,65 @@
+const squares = document.querySelectorAll('.square');
+let currentPlayer = 'X';
+const resultEl = document.querySelector('#result');
+const resetBtn = document.querySelector('#reset-btn');
+
+for (const square of squares) {
+  square.addEventListener('click', function() {
+    if (square.textContent === '') {
+      square.textContent = currentPlayer;
+      checkForWinningMove();
+      switchPlayer();
+    }
+  });
+}
+
+function switchPlayer() {
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkForWinningMove() {
+  const winningCombinations = [    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (const combination of winningCombinations) {
+    if (
+      squares[combination[0]].textContent === currentPlayer &&
+      squares[combination[1]].textContent === currentPlayer &&
+      squares[combination[2]].textContent === currentPlayer
+    ) {
+      resultEl.textContent = `Player ${currentPlayer} wins!`;
+      resultEl.style.color = 'green';
+      return;
+    }
+  }
+
+  let isDraw = true;
+  for (const square of squares) {
+    if (square.textContent === '') {
+      isDraw = false;
+      break;
+    }
+  }
+
+  if (isDraw) {
+    resultEl.textContent = 'Draw!';
+    resultEl.style.color = 'gray';
+  }
+}
+
+function resetGame() {
+  for (const square of squares) {
+    square.textContent = '';
+  }
+  currentPlayer = 'X';
+  resultEl.textContent = '';
+}
+
+resetBtn.addEventListener('click', resetGame);
