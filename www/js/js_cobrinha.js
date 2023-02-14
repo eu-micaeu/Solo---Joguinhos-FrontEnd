5b5f54a9c435c75802272f5fe77c5f6a9f9b7bc5
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-02-14 16:39:41 -0300

    by Micael

--- www/js/js_cobrinha.js
+++ www/js/js_cobrinha.js
@@ -2,15 +2,18 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const scoreDisplay = document.getElementById("score");
 
+// Tamanho da grade e contagem de tiles no canvas
 const gridSize = 20;
 const tileCount = canvas.width / gridSize;
 
+// Variáveis para armazenar a cobra, a maçã e a pontuação
 let snake = [{x: 10, y: 10}];
 let apple = {x: 15, y: 15};
 let dx = 0;
 let dy = 0;
 let score = 0;
 
+// Função para desenhar a cobra no canvas
 function drawSnake() {
   snake.forEach(segment => {
     ctx.fillStyle = "#333";
@@ -18,14 +21,18 @@ function drawSnake() {
   });
 }
 
+// Função para desenhar a maçã no canvas
 function drawApple() {
   ctx.fillStyle = "red";
   ctx.fillRect(apple.x * gridSize, apple.y * gridSize, gridSize, gridSize);
 }
 
+// Função para mover a cobra
 function moveSnake() {
+  // Cabeça da cobra é definida como sua primeira posição mais dx e dy
   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
   snake.unshift(head);
+  // Se a cabeça da cobra encontrar uma maçã, aumenta a pontuação e gera uma nova maçã
   if (head.x === apple.x && head.y === apple.y) {
     score++;
     scoreDisplay.textContent = "Pontuação: " + score;
@@ -35,15 +42,18 @@ function moveSnake() {
   }
 }
 
+// Função para gerar uma nova maçã
 function generateApple() {
   apple.x = Math.floor(Math.random() * tileCount);
   apple.y = Math.floor(Math.random() * tileCount);
+  // Verifica se a maçã foi gerada sobre a cobra
   snake.forEach(segment => {
     if (segment.x === apple.x && segment.y === apple.y) {
       generateApple();
     }
   });
 }
+
 
 function checkCollision() {
   if (snake[0].x < 0 || snake[0].x >= tileCount || snake[0].y < 0 || snake[0].y >= tileCount) {
