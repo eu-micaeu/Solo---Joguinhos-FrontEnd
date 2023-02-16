afed5dc36098144a34b0454a8efe9303b01e5e53
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-02-16 20:35:08 -0300

    by Micael

--- www/js/js_forca.js
+++ www/js/js_forca.js
@@ -10,7 +10,10 @@ const objects = ["cadeira", "mesa", "sofá", "abajur", "tapete"];
 // Array de cores
 const colors = ["vermelho", "azul", "amarelo", "verde", "roxo"];
 
-const randomLists = [times, fruits, objects, colors];
+// Array de nomes
+const names = ["fernando", "wellignton", "jesus", "micael", "mariana"];
+
+const randomLists = [times, fruits, objects, colors, names];
       const randomListIndex = Math.floor(Math.random() * randomLists.length);
       const randomList = randomLists[randomListIndex];
       const word = randomList[Math.floor(Math.random() * randomList.length)];
@@ -20,6 +23,8 @@ const randomLists = [times, fruits, objects, colors];
         listName = "Times";
       } else if (randomList === fruits) {
         listName = "Frutas";
+      } else if (randomList === names) {
+        listName = "Nomes";
       } else if (randomList === objects) {
         listName = "Objetos";
       } else {
@@ -43,13 +48,13 @@ let correct = [];
 
 // Cria um botão para cada letra do alfabeto
 for (let i = 97; i <= 122; i++) {
-const letter = String.fromCharCode(i);
-const button = document.createElement("button");
-button.textContent = letter;
-button.addEventListener("click", function() {
-handleGuess(letter);
-});
-letterButtonsContainer.appendChild(button);
+  const letter = String.fromCharCode(i);
+  const button = document.createElement("button");
+  button.textContent = letter;
+  button.addEventListener("click", function() {
+  handleGuess(letter);
+  });
+  letterButtonsContainer.appendChild(button);
 }
 
 // Cria uma array de underlines com o mesmo tamanho da palavra escolhida
@@ -62,13 +67,13 @@ wordElement.innerHTML = correct.join(" ");
 
 // Função chamada ao clicar em um botão de letra
 function handleGuess(letter) {
-if (wordArray.indexOf(letter) === -1) { // letra incorreta
-incorrect.push(letter); // adiciona a letra à array de incorretas
-incorrectElement.innerHTML = "Incorretas: " + incorrect.join(" "); // exibe as letras incorretas na tela
+  if (wordArray.indexOf(letter) === -1) { // letra incorreta
+  incorrect.push(letter); // adiciona a letra à array de incorretas
+  incorrectElement.innerHTML = "Incorretas: " + incorrect.join(" "); // exibe as letras incorretas na tela
 } else { // letra correta
-for (let i = 0; i < wordArray.length; i++) {
-if (wordArray[i] === letter) {
-correct[i] = word[i]; // atualiza a array de corretas com a letra correta na posição correta
+  for (let i = 0; i < wordArray.length; i++) {
+  if (wordArray[i] === letter) {
+  correct[i] = word[i]; // atualiza a array de corretas com a letra correta na posição correta
 }
 }
 wordElement.innerHTML = correct.join(" "); // exibe a nova array de corretas na tela
@@ -76,20 +81,20 @@ wordElement.innerHTML = correct.join(" "); // exibe a nova array de corretas na 
 
 // Verifica se o jogador venceu ou perdeu o jogo
 if (correct.join("") === word) { // se a array de corretas for igual à palavra, o jogador venceu
-guessElement.innerHTML = "Você ganhou!";
-guessElement.style.color = "green";
+  guessElement.innerHTML = "You Win!";
+  guessElement.style.color = "green";
 }
 
 if (incorrect.length === 4) { // se a array de incorretas tiver 6 letras, o jogador perdeu
-guessElement.innerHTML = "Você perdeu.";
-guessElement.style.color = "red";
-restartGame(); // reinicia o jogo
-}
+  guessElement.innerHTML = "You Lost";
+  guessElement.style.color = "red";
+  restartGame(); // reinicia o jogo
+  }
 }
 
 // Função para reiniciar o jogo
 function restartGame() {
-incorrect = [];
-correct = [];
-location.reload(); // recarrega a página
+  incorrect = [];
+  correct = [];
+  location.reload(); // recarrega a página
 }
