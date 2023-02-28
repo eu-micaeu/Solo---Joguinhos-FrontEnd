7f24e9dbaf63dc8a6693ff4eb6a6e57c5c37f417
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-02-28 18:16:39 -0300

    by Micael

--- js/js_memoria.js
+++ js/js_memoria.js
@@ -93,7 +93,7 @@ function flipCard(event) {
                     // Esconde a letra das cartas viradas incorretamente
                     card1.querySelector('.card-value').classList.remove('visible');
                     card2.querySelector('.card-value').classList.remove('visible');
-                }, 1000);
+                }, 0500);
             }
             checkIfWon(); // Adiciona a chamada para a função checkIfWon aqui
         }
